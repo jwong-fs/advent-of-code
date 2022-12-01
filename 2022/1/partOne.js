f6f5d76d2957c7e7ce0e1791196ddc9e566c89e1
Jenny Wong <jennyw981@gmail.com>
2022-12-01 01:05:33 -0500

    day 1 completed

---
+++ 2022/1/partOne.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+let rawInput = fs.readFileSync(path.resolve(__dirname, './input.txt'), 'utf8');
+
+const calories = rawInput.split('\n');
+
+let maxSoFar = -Infinity;
+let curr = 0;
+
+for (let calorie of calories) {
+  if (calorie === "") {
+    maxSoFar = Math.max(maxSoFar, curr);
+    curr = 0;
+  }  else {
+    curr += Number(calorie)
+  }
+}
+
+console.log(`The max amount of calories carried by an elf is: ${maxSoFar}`);
